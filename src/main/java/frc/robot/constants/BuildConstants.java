4ab77ddbf9ca12aa62d328419c2e442301177136
GrumpyBud <109627078+GrumpyBud@users.noreply.github.com>
2025-02-20 13:48:23 -0500

    update .gitignore

---
+++ src/main/java/frc/robot/constants/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.constants;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "robot-code-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 36;
+  public static final String GIT_SHA = "5b1a786df70ae00318ac91e0d2d3368d3360a8aa";
+  public static final String GIT_DATE = "2025-02-20 13:46:20 EST";
+  public static final String GIT_BRANCH = "customAutoBeta";
+  public static final String BUILD_DATE = "2025-02-20 13:47:40 EST";
+  public static final long BUILD_UNIX_TIME = 1740077260293L;
+  public static final int DIRTY = 1;
+
+  private BuildConstants(){}
+}
