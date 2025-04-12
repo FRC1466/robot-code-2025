a022bc67e211124716fa61d6283d4ee6a25b8cf1
GrumpyBud <109627078+GrumpyBud@users.noreply.github.com>
2025-04-12 19:49:05 -0400

    Looks scary, just changed spotless. If on branch starting with event, it will auto commit on each deploy

--- src/main/java/frc/robot/Main.java
+++ src/main/java/frc/robot/Main.java
@@ -1,6 +1,10 @@
-// Copyright (c) 2025 FRC Team 1466
-// https://github.com/FRC1466
- 
+// Copyright (c) 2025 FRC 1466
+// http://github.com/FRC1466
+//
+// Use of this source code is governed by an MIT-style
+// license that can be found in the LICENSE file at
+// the root directory of this project.
+
 package frc.robot;
 
 import edu.wpi.first.wpilibj.RobotBase;
